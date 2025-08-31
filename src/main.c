f39ce7f3bdf713f288bf9a92648f1fa89ed1694d
devsamuelv <realalloutmusic@gmail.com>
2025-08-31 00:29:57 -0400

    Working PWM channels

--- src/main.c
+++ src/main.c
@@ -1,18 +1,25 @@
 #include "ESP_CRSF.h"
 #include "esp_task_wdt.h"
-#include "driver/mcpwm.h";
-#include "driver/ledc.h";
+#include "driver/mcpwm.h"
+#include "driver/ledc.h"
 #include <esp_err.h>
 #include <math.h>
+#include <rtc_wdt.h>
+#include <stdlib.h>
+#include <esp_intr_alloc.h>
 
 // Arbitrary
-#define DUTY_RESOLUTION 5
-
+#define DUTY_RESOLUTION LEDC_TIMER_13_BIT
 #define LEDC_SPEED LEDC_HIGH_SPEED_MODE
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 
 float remap_channel_precent(int value)
 {
+  if (value == 0)
+  {
+    return 0;
+  }
+
   return (value - 922.0f) / 818.0f;
 }
 
@@ -24,12 +31,15 @@ void main_thread()
 
     CRSF_receive_channels(&channels);
 
-    // uint32_t desired_resolution = (channels.ch1);
-
     float speed_channel = remap_channel_precent(channels.ch3);
 
-    // Not a thread-safe method
-    ledc_set_duty(LEDC_SPEED, LEDC_CHANNEL, DUTY_RESOLUTION * speed_channel);
+    if (fabs(speed_channel) <= 0.05)
+    {
+      speed_channel = 0;
+    }
+
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_SPEED, LEDC_CHANNEL, 5046 * fabs(speed_channel)));
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_SPEED, LEDC_CHANNEL));
 
     printf("Channel 3 %f\n", speed_channel);
   }
@@ -40,14 +50,21 @@ void app_main()
 
   ledc_timer_config_t timer_config = {
       .speed_mode = LEDC_SPEED,
+      .timer_num = LEDC_TIMER_0,
       .duty_resolution = DUTY_RESOLUTION,
       .freq_hz = 50};
+  ESP_ERROR_CHECK(ledc_timer_config(&timer_config));
 
   ledc_channel_config_t channel_config = {
       .channel = LEDC_CHANNEL,
-      .gpio_num = 21,
-      .duty = (2 * DUTY_RESOLUTION),
+      .speed_mode = LEDC_SPEED,
+      .gpio_num = 19,
+      .intr_type = LEDC_INTR_DISABLE,
+      .timer_sel = LEDC_TIMER_0,
+      .hpoint = 0,
+      .duty = 0,
   };
+  ESP_ERROR_CHECK(ledc_channel_config(&channel_config));
 
   TaskHandle_t handle = NULL;
   crsf_config_t config = {
@@ -55,11 +72,9 @@ void app_main()
       .tx_pin = 17,
       .rx_pin = 16};
 
-  ledc_timer_config(&timer_config);
-  ledc_channel_config(&channel_config);
-
   CRSF_init(&config);
 
-  xTaskCreate(main_thread, "main", 3072, NULL, 12, NULL);
-  esp_task_wdt_add(handle);
+  xTaskCreate(main_thread, "main", 8192, NULL, 12, &handle);
+
+  ESP_ERROR_CHECK(esp_task_wdt_add(handle));
 }
